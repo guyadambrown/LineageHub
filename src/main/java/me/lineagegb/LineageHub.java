6fafdec20afff08fd49d8e1f907a6a38a1614f31
LineageGH <72948759+LineageGH@users.noreply.github.com>
2021-03-14 20:21:01 +0000

    Initial commit

---
+++ src/main/java/me/lineagegb/LineageHub.java
@@ -0,0 +1,17 @@
+package me.lineagegb;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class LineageHub extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
